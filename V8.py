c0de6543faca310922bb1e59ffe358c2126ee825
MuhammadAbdullah979 <muhammadabdullahraja463@gmail.com>
2026-09-25 19:30:18 +0300

    Add grocery list functionality
    Implemented a grocery list maker that allows users to add items.

--- V8.py
+++ V8.py
@@ -1,1 +1,19 @@
-https://www.programiz.com/online-compiler/1V4C7rF6OGJxH
+grocery_list=[]
+print("Welcome to the grocery list maker!")
+#Add items
+while True:
+    choice=input('Do you want to add something to the list(Y/N): ')
+    choice = choice.strip()
+    choice = choice.upper()
+    if choice == 'X':
+        break
+    if choice == 'Y':
+        yes_command = input("Enter the name of the item you want to add you want to add: ")
+        yes_command = yes_command.strip()
+        grocery_list.append(yes_command)
+    elif choice == 'N':
+        break
+    else:
+        print("Wrong input recieved")
+print(grocery_list)
+
